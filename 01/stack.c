2274bab634779c1a46f3917971f406757d083a9c
wencakisa <wencakisa@gmail.com>
2017-09-28 19:58:08 +0300

    Added stack resize function

--- 01/stack.c
+++ 01/stack.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define DEF_CAP 20
+#define MAX_CAPACITY 100
 
 typedef struct {
     size_t size;
@@ -14,24 +14,23 @@ void stack_destroy(stack*);
 
 int stack_empty(stack*);
 int stack_full(stack*);
+
 int stack_top(stack*);
+
+void stack_resize(stack*);
 
 void stack_push(stack*, int);
 void stack_pop(stack*);
 
-int main(int argc, char const *argv[]) {
+int main() {
     stack st;
     stack_init(&st);
 
-    for (size_t i = 0; i < st.capacity; i++) {
+    for (size_t i = 0; i < MAX_CAPACITY + 5; i++) {
         stack_push(&st, i);
-        printf("Pushed: %d\n", stack_top(&st));
     }
 
-    for (size_t i = 0; i < st.capacity; i++) {
-        printf("Popped: %d\n", stack_top(&st));
-        stack_pop(&st);
-    }
+    printf("Top: %d\n", stack_top(&st));
 
     stack_destroy(&st);
 
@@ -40,7 +39,7 @@ int main(int argc, char const *argv[]) {
 
 void stack_init(stack* st) {
     st->size = 0;
-    st->capacity = DEF_CAP;
+    st->capacity = MAX_CAPACITY;
     st->elements = (int*) calloc(st->capacity, sizeof(int));
 }
 
@@ -63,14 +62,21 @@ int stack_top(stack* st) {
     return stack_empty(st) ? -1 : st->elements[st->size - 1];
 }
 
+void stack_resize(stack* st) {
+    st->capacity *= 2;
+    st->elements = (int*) realloc(st->elements, st->capacity * sizeof(int));
+}
+
 void stack_push(stack* st, int n) {
-    if (!stack_full(st)) {
-        st->elements[st->size++] = n;
+    if (stack_full(st)) {
+        stack_resize(st);
     }
+
+    st->elements[st->size++] = n;
 }
 
 void stack_pop(stack* st) {
     if (!stack_empty(st)) {
-        st->elements[st->size--] = 0;
+        st->size--;
     }
 }
