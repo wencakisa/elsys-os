aad6fc00ed30329843d62bd09d6c44d5499f0360
wencakisa <wencakisa@gmail.com>
2017-10-02 10:58:24 +0300

    Added first task of second lecture

---
+++ 02/bmp-info.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <string.h>
+#include "read.h"
+
+#define FILE_ARGV_INDEX 1
+#define FILE_WIDTH_BYTE 18
+#define FILE_HEIGHT_BYTE 22
+
+int main(int argc, char const *argv[]) {
+    const char *filename = argv[FILE_ARGV_INDEX];
+
+    char *file = (char*) read_image(filename);
+
+    int32_t width;
+    // Create a 4-byte variable that will store the width
+
+    memcpy(&width, file + FILE_WIDTH_BYTE, sizeof(width));
+    // file + FILE_WIDTH_BYTE -> pointer to the (FILE_WIDTH_BYTE)th byte of the file
+    // sizeof(width) = sizeof(int32_t) = 4
+    // Copy into width 4 bytes after the (FILE_WIDTH_BYTE)th byte of the file!
+
+    int32_t height;
+    memcpy(&height, file + FILE_HEIGHT_BYTE, sizeof(height));
+    // file + FILE_HEIGHT_BYTE -> pointer to the (FILE_HEIGHT_BYTE)th byte of the file
+    // sizeof(width) = sizeof(int32_t) = 4
+    // Copy into width 4 bytes after the (FILE_HEIGHT_BYTE)th byte of the file!
+
+    printf("BMP image width: %d\n", width);
+    printf("BMP image height: %d\n", height);
+
+    return 0;
+}
---
+++ 02/read.h
@@ -0,0 +1,6 @@
+#ifndef READ_H
+#define READ_H
+
+void *read_image(const char*);
+
+#endif
