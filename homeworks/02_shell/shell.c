d05f3cce98fcc4a7881efb94b52cc2e2da559358
wencakisa <wencakisa@gmail.com>
2018-01-02 11:50:11 +0200

    Shell

--- homeworks/02_shell/shell.c
+++ homeworks/02_shell/shell.c
@@ -1,3 +1,14 @@
+//------------------------------------------------------------------------
+// NAME: Vencislav Tashev
+// CLASS: XIa
+// NUMBER: 4
+// PROBLEM: #2
+// FILE NAME: shell.c
+// FILE PURPOSE:
+// A Unix-like shell command interpreter
+// written in C using processes and exec* functions
+//------------------------------------------------------------------------
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -10,8 +21,10 @@
 #define BUFF_SIZE 4096
 #define DELIMITER " "
 
-char** parse_cmdline(const char* cmdline);
 void free_string_array(char** arr);
+size_t count(const char* haystack, const char needle);
+char** parse_cmdline(const char* cmdline);
+void run_command(char** command);
 
 int main() {
     char buffer[BUFF_SIZE];
@@ -29,41 +42,54 @@ int main() {
         // returning the number of characters of str1 read before this first occurrence.
         buffer[strcspn(buffer, "\n")] = '\0';
 
-        char** parsed_line = parse_cmdline(buffer);
-        const char* path = parsed_line[0];
-
-        pid_t pid = fork();
-
-        if (pid == 0) {
-            execvp(path, parsed_line);
-            // exec* will interrupt the child process
-            // so the error handling can be placed immediately after exec*
-            // when exec* fails, it will continue down the child process
-            // and the error handling will execute correctly
-
-            perror(path);
-            free_string_array(parsed_line);
-            _exit(EXIT_FAILURE);
-        } else if (pid > 0) {
-            wait(NULL);
-            free_string_array(parsed_line);
-        } else {
-            perror("fork");
-            free_string_array(parsed_line);
-            exit(EXIT_FAILURE);
-        }
+        // Running the already parsed command line
+        run_command(parse_cmdline(buffer));
     }
 }
 
+//------------------------------------------------------------------------
+// FUNCTION: free_string_array
+// Completely frees space for a dynamically allocated char** (array of strings)
+// PARAMETERS:
+// char** arr - The array to be freed
+//------------------------------------------------------------------------
 void free_string_array(char** arr) {
     for (int i = 0; arr[i] != '\0'; i++) {
-        // clear each previously allocated element in the array
+        // free each previously allocated element in the array
         free(arr[i]);
     }
-    // clear the whole array pointer
+    // free the whole array pointer
     free(arr);
+    // set the array pointer to point to NULL
+    arr = NULL;
 }
 
+//------------------------------------------------------------------------
+// FUNCTION: count
+// Counts a char encounters in a string
+// PARAMETERS:
+// const char* haystack - The string that we search in
+// const char needle    - The char that we search for
+//------------------------------------------------------------------------
+size_t count(const char* haystack, const char needle) {
+    size_t result = 0;
+    for (size_t i = 0; i < strlen(haystack); i++) {
+        if (haystack[i] == needle) {
+            result++;
+        }
+    }
+
+    return result;
+}
+
+//------------------------------------------------------------------------
+// FUNCTION: parse_cmdline
+// Parses a provided line from the input
+// by tokenizing it by space and
+// returning the tokens in an dynamically allocated array
+// PARAMETERS:
+// const char* cmdline - The line to be parsed
+//------------------------------------------------------------------------
 char** parse_cmdline(const char* cmdline) {
     const size_t len = strlen(cmdline);
 
@@ -72,24 +98,68 @@ char** parse_cmdline(const char* cmdline) {
     char* line_copy = (char*) calloc(sizeof(char), len + 1);
     strcpy(line_copy, cmdline);
 
-    // the result array of strings (char**)
-    char** parsed = (char**) calloc(sizeof(char*), len);
+    // counting the encounters of ' ' in the string in order to determine
+    // the size for the result array of strings
+    const size_t spaces_count = count(line_copy, ' ');
+    // the result array of strings
+    // we allocate spaces_count + 2 memory for the null byte
+    char** parsed = (char**) calloc(sizeof(char*), spaces_count + 2);
 
     // strtok() breaks provided string into copies by provided delimiter
     char* token = strtok(line_copy, DELIMITER);
     int i = 0;
     for (; token != NULL; i++) {
         // allocate space for the current element in the result array
+        // and copying the current token value into it
         parsed[i] = (char*) calloc(sizeof(char), strlen(token) + 1);
-        // copy the token value into it
         strcpy(parsed[i], token);
 
+        // go to the next token
         token = strtok(NULL, DELIMITER);
     }
     // set a null byte in the end
     parsed[i] = '\0';
 
+    // free the line copy because we do not need it anymore
     free(line_copy);
 
     return parsed;
+}
+
+//------------------------------------------------------------------------
+// FUNCTION: run_command
+// The main functionality of the program
+// Runs a given command array of strings ( e.g. {"ls", "-la"} ) by:
+// -> creating a process ( fork() ),
+// -> executing the already parsed command in child process ( exec*() )
+// PARAMETERS:
+// char** command - The command line to be executed
+//------------------------------------------------------------------------
+void run_command(char** command) {
+    // create a new process using fork()
+    pid_t pid = fork();
+
+    if (pid == 0) {
+        // Child process
+
+        execvp(command[0], command);
+        // exec* will interrupt the child process
+        // so the error handling can be placed immediately after exec*
+        // when exec* fails, it will continue down the child process
+        // and the error handling will execute correctly
+
+        perror(command[0]);
+        free_string_array(command);
+        _exit(EXIT_FAILURE);
+    } else if (pid > 0) {
+        // Parent process
+
+        // Wait for the child to finish it's work
+        waitpid(pid, NULL, 0);
+    } else {
+        perror("fork");
+    }
+
+    // Free the already executed command, we do not need it anymore
+    free_string_array(command);
 }
